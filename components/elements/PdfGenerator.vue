528939c3c47011c8a42be43c9f6dc1e853b138f1
yubathom <yubathom@gmail.com>
2020-02-13 19:29:59 -0300

    refactor: reduz bundle de pdfmaker. De 2mb para 45kb

--- components/elements/PdfGenerator.vue
+++ components/elements/PdfGenerator.vue
@@ -1,14 +1,10 @@
 <template>
   <button @click.prevent="printPage">
     <span>&DownArrowBar;</span>
-    Salvar como .pdf
+    {{ message }}
   </button>
 </template>
 <script>
-import pdfMake from 'pdfmake/build/pdfmake'
-import pdfFonts from 'pdfmake/build/vfs_fonts'
-pdfMake.vfs = pdfFonts.pdfMake.vfs
-
 export default {
   name: 'PdfGenerator',
   props: {
@@ -17,11 +13,39 @@ export default {
       required: true
     }
   },
+  data () {
+    return {
+      isLoading: false,
+      message: 'Salvar como .pdf'
+    }
+  },
   methods: {
+    loadExternalLib (url) {
+      return new Promise((resolve, reject) => {
+        const script = document.createElement('script')
+        script.onload = () => { resolve(`${url} está pronta`) }
+        script.async = true
+        script.src = url
+        document.head.appendChild(script)
+      })
+    },
     printPage () {
-      const dateStr = new Date().toISOString().slice(0, 19)
-      const fileName = `${this.$route.path.slice(1).replace('/', '-')}_${dateStr}.pdf`
-      pdfMake.createPdf(this.pdfDocDefinition).download(fileName)
+      this.isLoading = true
+      this.message = 'Criando arquivo'
+      this.loadExternalLib('https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.62/pdfmake.min.js')
+        .then(() => {
+          this.loadExternalLib('https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.62/vfs_fonts.js')
+            .then(() => {
+              const dateStr = new Date().toISOString().slice(0, 19)
+              const fileName = `${this.$route.path.slice(1).replace('/', '-')}_${dateStr}.pdf`
+              window.pdfMake.createPdf(this.pdfDocDefinition).download(fileName)
+            })
+        })
+        .catch(err => console.error(err))
+        .finally(() => {
+          this.isLoading = false
+          this.message = 'Salvar como .pdf'
+        })
     }
   }
 }
