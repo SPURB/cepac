4f83751a5f581def252713bf306587cb9e51e995
m-dantas <mauriciodantas.contato01@bol.com.br>
2020-03-05 18:32:13 -0300

    feat: add component CustomSelect

---
+++ components/elements/CustomSelect.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="form-section">
+    <div class="title-select">
+      {{ buildSelect.title }}
+    </div>
+    <div class="custom-select">
+      <select>
+        <option selected disabled>
+          Selecione uma opção
+        </option>
+        <option
+          :key="item.name+index"
+          v-for="(item, index) in buildSelect.values"
+          :value="item.value"
+        >
+          {{ item.name }}
+        </option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomSelect',
+  props: {
+    buildSelect: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/variables";
+.form-section {
+  display: flex;
+  flex-direction: column;
+  margin: 15px;
+  width: 33%;
+
+  .title-select {
+    color: #fff;
+    font-size: 17pt;
+    float: rigth;
+  }
+
+  .custom-select {
+    position: relative;
+    display: block;
+    margin: 0 auto;
+    background-color: $brand-1;
+    z-index: 1;
+    border-radius: 5px;
+    width: 100%;
+
+    select {
+      appearance: none;
+      border: none;
+      background: transparent;
+      font-size: 14px;
+      color: #fff;
+      cursor: pointer;
+      display: block;
+      outline: none;
+      border-radius: 0;
+      margin: 0;
+      width: 100%;
+      padding: 12px 55px 15px 15px;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+
+      option {
+        box-sizing: inherit;
+        background-color: $brand-1;
+        cursor: pointer;
+        font-size: 14px;
+      }
+    }
+
+    &:after {
+      border-left: 1px solid #fff;
+      color: #fff;
+      content: "\2228";
+      font-size: 15px;
+      position: absolute;
+      line-height: 38px;
+      text-align: center;
+      top: 0;
+      right: 0;
+      width: 30px;
+      height: 100%;
+      z-index: -1;
+    }
+  }
+}
+</style>
