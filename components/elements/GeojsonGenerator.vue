9ad30125b4b8a16f097c6fe7191e0898c8530664
Maurício Dantas <45770805+m-dantas@users.noreply.github.com>
2020-09-01 16:20:59 -0300

    feat: inclui geração de .png e .geojson (#12)

---
+++ components/elements/GeojsonGenerator.vue
@@ -0,0 +1,29 @@
+<template>
+  <button @click.prevent="createGeojson(fileName, geojsonDocDefinition)">
+    <span style="font-size: 0.8rem">{ }</span>
+    Salvar como .geojson
+  </button>
+</template>
+<script>
+import FileSaver from 'file-saver'
+
+export default {
+  name: 'GeojsonGenerator',
+  props: {
+    fileName: {
+      required: true,
+      type: String
+    },
+    geojsonDocDefinition: {
+      type: undefined,
+      required: true
+    }
+  },
+  methods: {
+    createGeojson (name, content) {
+      const geojsonBlob = new Blob([JSON.stringify(content)], { type: 'text/geojson; charset=utf-8' })
+      FileSaver.saveAs(geojsonBlob, name)
+    }
+  }
+}
+</script>
